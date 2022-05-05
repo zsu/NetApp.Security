7fb6bfc014d22054522da9482b6e24ca4616d6aa
zsu <azure.ss@gmail.com>
2022-05-04 21:40:11 -0400

    Refactor

---
+++ NetApp.Security.Windows/Ldap/ILdapService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NetApp.Security.Windows
+{
+    public interface ILdapService: NetApp.Security.ILdapService
+    {
+
+        void UpdatePhoto(string username, Stream stream);
+    }
+}
