8e0e0f2224a49e2e51f25ee82581dcdc0fbb4380
zsu <azure.ss@gmail.com>
2022-12-14 17:54:40 -0500

    Remove Novell Ldap dependency

--- NetApp.Security/Ldap/Model/LdapSettings.cs
+++ NetApp.Security/Ldap/Model/LdapSettings.cs
@@ -22,7 +22,8 @@
         public string DomainName { get; set; }
 
         public string DomainDistinguishedName { get; set; }
-
+        public int PageSize { get; set; } = 1000;
         public LdapCredentials Credentials { get; set; }
+        public bool ReferralFollowing { get; set; }
     }
 }
